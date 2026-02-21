7dd45a28c6bc9b1d64e62279d71604c90a892aac
Pavel Pasaz <pavel@example.com>
2026-02-21 17:16:32 +0300

    delete py sheets

--- pysheets/src/db/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модуль работы с базой данных SQLite - упрощённая версия
-"""
-
-from .database_manager import DatabaseManager
-from .models import Spreadsheet, SheetFunction, RecentFile
-
-__all__ = ['DatabaseManager', 'Spreadsheet', 'SheetFunction', 'RecentFile']
