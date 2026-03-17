d21bb15282637a015f9d96f00c98f0175a737388
Pavel Pasaz <pavel@example.com>
2026-03-17 17:15:43 +0300

    Rename folder electron -> SmartTable

--- sheets/src/electron/dist/database/database.js
+++
@@ -1,6 +0,0 @@
-/**
- * Database Manager - управление локальной базой данных без внешних зависимостей
- */
-// Re-export из simple-database.ts для совместимости
-export { databaseManager, SimpleDatabaseManager as DatabaseManager } from './simple-database.js';
-//# sourceMappingURL=database.js.map
